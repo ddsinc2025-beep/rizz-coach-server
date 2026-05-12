f615991b3288ff1c41f27466cc724dbedbc4db60
ddsinc2025-beep <ddsinc2025@gmail.com>
2026-05-11 21:03:38 -0400

    Update server.js

--- server.js
+++ server.js
@@ -40,5 +40,9 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+app.get('/genie', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
